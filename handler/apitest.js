4d9cf86e0417e56c4aa79f9c4399d79f9c7e6a05
Jack Wrld <107874128+batm9n@users.noreply.github.com>
2023-10-12 00:46:02 +0700

    Revert "feat(money-stream): Add Vesting/Payment using Streamflow (#17)"
    This reverts commit 256c6ccae424130c65647ad647be31bbc0336867.

--- handler/apitest.js
+++ handler/apitest.js
@@ -1,27 +1,27 @@
 // TODO:
-const axios = require("axios"); // legacy way/**
+const axios = require('axios'); // legacy way/**
 /*From onchainId, check if it is listed. If listed, return its metadata(sellerId, grossAmount) If not listed, return  empty value
  * @param onchainId: Id of the NFT
  * @param txs: Metadata of listing NFT in a particular collection
  */
 
 function retriveMintInfo(onchainId, txs) {
-    for (let i = 0; i < txs.length; i++) {
-        if (txs[i].mint.onchainId == onchainId) {
-            console.log("Found given NFT on listing");
-            console.log(onchainId);
-            return;
-        }
+  for (let i = 0; i < txs.length; i++) {
+    if (txs[i].mint.onchainId == onchainId) {
+      console.log("Found given NFT on listing");
+      console.log(onchainId);
+      return;
     }
-    console.log("NFT not found");
+  }
+  console.log("NFT not found");
 }
 // Get the lowest price nft in the collection
 (async () => {
-    try {
-        const { data } = await axios.post(
-            "https://api.tensor.so/graphql",
-            {
-                query: `
+  try {
+    const { data } = await axios.post(
+      "https://api.tensor.so/graphql",
+      {
+        query: `
         query ActiveListingsV2(
             $slug: String!
             $sortBy: ActiveListingsSortBy!
@@ -54,34 +54,35 @@ function retriveMintInfo(onchainId, txs) {
               }
             }
           }`,
-                variables: {
-                    slug: "a12fcd5d-2067-4e10-8ca2-e7ab4b342e32",
-                    sortBy: "PriceAsc",
-                    filters: {
-                        sources: ["TENSORSWAP", "TCOMP"],
-                    },
-                    limit: 1, // Max: 250
-                    // To get more results, pass `page.endCursor` from the response
-                    cursor: null,
-                },
-            },
-            {
-                headers: {
-                    "Content-Type": "application/json",
-                    "X-TENSOR-API-KEY": process.env.TENSOR_API_KEY ?? "",
-                },
-            }
-        );
+        variables: {
+          slug: "a12fcd5d-2067-4e10-8ca2-e7ab4b342e32",
+          sortBy: "PriceAsc",
+          filters: {
+            sources: ["TENSORSWAP", "TCOMP"],
+          },
+          limit: 1, // Max: 250
+          // To get more results, pass `page.endCursor` from the response
+          cursor: null,
+        },
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "X-TENSOR-API-KEY":
+            process.env.TENSOR_API_KEY ??
+            "",
+        },
+      }
+    );
 
-        // Query response (if no errors)
-        retriveMintInfo(
-            "77mmcFGGqdXjjXH1xjU8sZa2Ty3Cd3M7QwoEvkjii5at",
-            data.data.activeListingsV2.txs
-        );
-    } catch (err) {
-        // Any errors.
-        if (err instanceof axios.AxiosError)
-            console.log(err.response?.data.errors);
-        else console.error(err);
-    }
+    // Query response (if no errors)
+    retriveMintInfo(
+      "77mmcFGGqdXjjXH1xjU8sZa2Ty3Cd3M7QwoEvkjii5at",
+      data.data.activeListingsV2.txs
+    );
+  } catch (err) {
+    // Any errors.
+    if (err instanceof axios.AxiosError) console.log(err.response?.data.errors);
+    else console.error(err);
+  }
 })();
--- handler/transpiler.js
+++ handler/transpiler.js
@@ -6,274 +6,191 @@
 
 // train word 2 vec model on the given txt file and the find the similarity
 const {
-    nftInfoExtracter,
-    supportedNFTs,
-    swapInfoExtractor,
-    sendTokenInfoExtractor,
-    supportTokenSend,
-    supportedTokenSwap,
-    bridgeInfoExtractor,
-    supportedTokenSwapSolana,
-    supportedTensorCollection,
-    tensorInfoExtracter,
-    tensorSwapQuery,
-    streamInfoExtractor,
-    supportedTokenStream,
+  nftInfoExtracter,
+  supportedNFTs,
+  swapInfoExtractor,
+  sendTokenInfoExtractor,
+  supportTokenSend,
+  supportedTokenSwap,
+  bridgeInfoExtractor,
+  supportedTokenSwapSolana,
+  supportedTensorCollection,
+  tensorInfoExtracter,
+  tensorSwapQuery
 } = require("./constants");
-const { getResponse } = require("./gpt/llm");
-const { isWordSimilar, isPairSimilar } = require("./utils/similarity");
-const { constructNFTransaction } = require("./transactions/nftTransactions");
+const { getResponse } = require('./gpt/llm')
+const { isWordSimilar, isPairSimilar } = require('./utils/similarity')
+const { constructNFTransaction } = require('./transactions/nftTransactions');
 const { constructSendTransaction } = require("./transactions/tokenTransaction");
-const { constructSwapTransaction } = require("./transactions/swapTransaction");
-const {
-    constructStreamTransaction,
-} = require("./transactions/streamTransaction");
-const {
-    contructBridgeTransactionForStaking,
-} = require("./transactions/bridgeAndStake");
+const { constructSwapTransaction } = require('./transactions/swapTransaction')
+const { contructBridgeTransactionForStaking } = require('./transactions/bridgeAndStake');
 const tensorQuery = require("./utils/tensorQuery");
 
 const transpiler = async (currentStep, classifier, userAddress, chain) => {
-    console.log("this is chain ", chain);
-    console.log("this is chain ", typeof chain);
-    console.log("this is chain ", chain === 137);
-    const context = classifier.classify(currentStep);
-    console.log("step context ", context);
 
-    // polygon mainnet and gnosis
-    if (context === "swap") {
-        const swapInfo = [];
+  console.log('this is chain ', chain)
+  console.log('this is chain ', typeof chain)
+  console.log('this is chain ', chain === 137)
+  const context = classifier.classify(currentStep);
+  console.log('step context ', context);
 
-        for (let i = 0; i < swapInfoExtractor.length; i++) {
-            const resp = await getResponse(
-                swapInfoExtractor[i].question,
-                currentStep
-            );
-            if (i === 0) swapInfo.push(JSON.parse(resp));
-            else swapInfo.push(resp);
-        }
+  // polygon mainnet and gnosis
+  if (context === "swap") {
+    const swapInfo = [];
+    
+    for(let i = 0; i < swapInfoExtractor.length; i++) {
+      const resp = await getResponse(swapInfoExtractor[i].question, currentStep);
+      if(i === 0) 
+      swapInfo.push(JSON.parse(resp))
+      else
+      swapInfo.push(resp);
+    };
 
-        console.log(swapInfo);
+    console.log(swapInfo)
 
-        let swapMeta;
-        // order would always be correct in supportedTokenSwap
+    let swapMeta;
+    // order would always be correct in supportedTokenSwap
+    
+    // if(chain === "137") {
+    //   console.log('we are here ', chain);
+    //   swapMeta = supportedTokenSwap.filter(pair => isPairSimilar(pair.pair, swapInfo[0]));
+    //   console.log('these are supportred tokens ', supportedTokenSwap);
+    // }
+    // else 
+    swapMeta = supportedTokenSwapSolana.filter(pair => isPairSimilar(pair.pair, swapInfo[0]));
 
-        // if(chain === "137") {
-        //   console.log('we are here ', chain);
-        //   swapMeta = supportedTokenSwap.filter(pair => isPairSimilar(pair.pair, swapInfo[0]));
-        //   console.log('these are supportred tokens ', supportedTokenSwap);
-        // }
-        // else
-        swapMeta = supportedTokenSwapSolana.filter((pair) =>
-            isPairSimilar(pair.pair, swapInfo[0])
-        );
+    if(swapMeta.length === 0) return "Insufficient details for swap";
+    console.log('this is swap meta ', swapMeta);
 
-        if (swapMeta.length === 0) return "Insufficient details for swap";
-        console.log("this is swap meta ", swapMeta);
-
-        let singlePair = swapMeta[0];
-        // Naive check if the pair should be in reverse order
-        // because maybe the gpt pair response is in reverse order
-        if (singlePair.pair[0].toLowerCase() === swapInfo[0][0].toLowerCase()) {
-            singlePair = {
-                ...singlePair,
-                pair: [singlePair.pair[1], singlePair.pair[0]],
-            };
-        }
-        console.log(singlePair);
-
-        const swapTransactionMeta = {
-            pair: singlePair.pair,
-            tokenIn: singlePair[singlePair.pair[0]],
-            tokenOut: singlePair[singlePair.pair[1]],
-            amount: swapInfo[1],
-            userAddress,
-            chain,
-        };
-
-        const resp = await constructSwapTransaction(swapTransactionMeta);
-        console.log(" this is resp ", resp);
-        return { ...resp, type: "swap" };
-    } else if (context === "nft_buy") {
-        // Can you purchase me 1 Drop Nation DRiP NFT ?
-        // nftInfoExtracter
-        console.log("extracting info");
-
-        const nftInfo = [];
-
-        for (let i = 0; i < tensorInfoExtracter.length; i++) {
-            const resp = await getResponse(
-                tensorInfoExtracter[i].question,
-                currentStep
-            );
-            nftInfo.push(resp);
-        }
-
-        // 0 -> name
-        // 1 -> operation
-        // 2 -> tokenId
-        // 3 -> to
-        const data = supportedTensorCollection.filter((d) =>
-            isWordSimilar(d.name, nftInfo[0])
-        );
-        console.log(data);
-        console.log(nftInfo[0]);
-        if (data.length === 0) return "Insufficient details for nft operation";
-
-        // Get query info for API call
-        const nftMeta = data[0];
-        const collectionSlug = nftMeta.slug;
-        const activeListingsQuery = tensorSwapQuery.activeOrders.query;
-        const activeListingVariable = tensorSwapQuery.activeOrders.variable;
-        activeListingVariable["slug"] = collectionSlug;
-
-        const res = await tensorQuery(
-            activeListingsQuery,
-            activeListingVariable
-        );
-        console.log(res);
-        // 0 -> cheapest NFT available
-        const tokenId = res.activeListingsV2.txs[0].mint.onchainId;
-        const sellerId = res.activeListingsV2.txs[0].tx.sellerId;
-        const grossAmount = res.activeListingsV2.txs[0].tx.grossAmount; // In lamports
-        const nftTransactionData = {
-            operation: "buy",
-            name: nftMeta.name,
-            slug: nftMeta.slug,
-            tokenId: tokenId,
-            owner: sellerId,
-            amount: grossAmount,
-            userAddress: userAddress,
-            // toAddress: nftInfo[3],
-        };
-
-        const resp = await constructNFTransaction(nftTransactionData);
-        console.log("this is resp", resp);
-
-        return { ...resp, type: "nft_buy" };
-    } else if (context === "nft_sell") {
-        // TODO:
-    } else if (context == "stream") {
-        // streamInfoExtractor
-        console.log("extracting info");
-
-        const streamInfo = [];
-
-        for (let i = 0; i < streamInfoExtractor.length; i++) {
-            const resp = await getResponse(
-                streamInfoExtractor[i].question,
-                currentStep
-            );
-            streamInfo.push(resp);
-        }
-
-
-        // // Dummy data
-        // const streamInfo = [
-        //     "SOL",
-        //     "4WMjxRZ1HhX4RhZ1fiohpwUTmjeCudQhwYogzvqHKSjh",
-        //     1,
-        //     "payment",
-        //     "second",
-        //     '["3", "months"]',
-        // ];
-        console.log(streamInfo);
-        // 0 -> token name
-        // 1 -> recipent address
-        // 2 -> token amount
-        // 3 -> type of stream
-        // 4 -> unlocking interval (enftInfovery second, minute,...)
-        // 5 -> streaming duration -> 0: number, 1: duration(second, minute,...)
-        const tokenData = supportedTokenStream.filter((d) =>
-            isWordSimilar(d.name, streamInfo[0])
-        );
-        if (tokenData.length === 0)
-            return "Insufficient details for stream operation";
-        console.log(tokenData[0].address)
-        const streamTransactionData = {
-            operation: "stream",
-            name: streamInfo[0],
-            recipent: streamInfo[1],
-            tokenId: tokenData[0].address,
-            amount: streamInfo[2],
-            streamType: streamInfo[3],
-            unlockInterval: streamInfo[4],
-            streamDuration: JSON.parse(streamInfo[5]),
-            programId: tokenData[0].programId.devnet, // TODO: add support for choose cluster
-            // toAddress: nftInfo[3],
-        };
-
-        const resp = await constructStreamTransaction(streamTransactionData);
-        console.log("this is resp", resp);
-
-        return { ...resp, type: "stream" };
-    } else if (context === "transfer") {
-        // polygon testnet
-        console.log("extracting transfer info");
-
-        const tokenSendInfo = [];
-        for (let i = 0; i < sendTokenInfoExtractor.length; i++) {
-            const resp = await getResponse(
-                sendTokenInfoExtractor[i].question,
-                currentStep
-            );
-            tokenSendInfo.push(resp);
-        }
-
-        // 0 -> name
-        // 1 -> amount
-        // 2 -> to address
-        const sendTokenMeta = supportTokenSend.filter((data) =>
-            isWordSimilar(data.name, tokenSendInfo[0])
-        );
-
-        if (sendTokenMeta.length === 0)
-            return "Insufficient details for transfer";
-
-        const sendTransactionData = {
-            programAddress: sendTokenMeta[0].address,
-            name: sendTokenMeta[0].name,
-            fromAddress: userAddress,
-            amount: tokenSendInfo[1],
-            toAddress: tokenSendInfo[2],
-        };
-
-        console.log("sendTransactionData", sendTransactionData);
-        const sendTransactionResp = await constructSendTransaction(
-            sendTransactionData
-        );
-        console.log("sendTransactionResp", sendTransactionResp);
-
-        return { ...sendTransactionResp, type: "transfer" };
-    } else if (context === "staking") {
-        // mumbai -> avalanche
-        let tokenAmount = 0;
-        // 0 -> token amount
-        tokenAmount = await getResponse(
-            bridgeInfoExtractor[0].question,
-            currentStep
-        );
-
-        if (tokenAmount === "-") {
-            return {
-                success: false,
-                transactions: [],
-            };
-        }
-
-        const bridgeTransactionData = {
-            amount: tokenAmount,
-            userAddress,
-        };
-
-        console.log("bridge data ", bridgeTransactionData);
-
-        let bridgeTxnResp = await contructBridgeTransactionForStaking(
-            bridgeTransactionData
-        );
-        console.log("txn ", bridgeTxnResp);
-        return { ...bridgeTxnResp, type: "bridge" };
+    let singlePair = swapMeta[0]
+    // Naive check if the pair should be in reverse order 
+    // because maybe the gpt pair response is in reverse order
+    if (singlePair.pair[0].toLowerCase() === swapInfo[0][0].toLowerCase()) {
+      singlePair = {
+        ...singlePair, 
+        pair: [singlePair.pair[1], singlePair.pair[0]],
+      }
     }
+    console.log(singlePair)
+    
+    const swapTransactionMeta = {
+      pair: singlePair.pair,
+      tokenIn: singlePair[singlePair.pair[0]],
+      tokenOut: singlePair[singlePair.pair[1]],
+      amount: swapInfo[1],
+      userAddress,
+      chain
+    };
+
+    const resp = await constructSwapTransaction(swapTransactionMeta);
+    console.log(' this is resp ', resp);
+    return { ...resp, type: 'swap' };
+
+  } else if (context === "nft_buy") { // Can you purchase me 1 Drop Nation DRiP NFT ?
+    // nftInfoExtracter
+    console.log('extracting info');
+
+    const nftInfo = [];
+
+    for(let i=0; i < tensorInfoExtracter.length; i++) {
+        const resp = await getResponse(tensorInfoExtracter[i].question, currentStep);
+        nftInfo.push(resp);
+    }
+
+    // 0 -> name 
+    // 1 -> operation
+    // 2 -> tokenId
+    // 3 -> to  
+    const data = supportedTensorCollection.filter(d => isWordSimilar(d.name, nftInfo[0]));
+    console.log(data)
+    console.log(nftInfo[0])
+    if(data.length === 0) return "Insufficient details for nft operation";
+
+    // Get query info for API call
+    const nftMeta = data[0]
+    const collectionSlug = nftMeta.slug
+    const activeListingsQuery = tensorSwapQuery.activeOrders.query;
+    const activeListingVariable = tensorSwapQuery.activeOrders.variable
+    activeListingVariable["slug"] = collectionSlug
+
+    const res = await tensorQuery(activeListingsQuery, activeListingVariable);
+    console.log(res)
+    // 0 -> cheapest NFT available
+    const tokenId = res.activeListingsV2.txs[0].mint.onchainId;
+    const sellerId = res.activeListingsV2.txs[0].tx.sellerId
+    const grossAmount = res.activeListingsV2.txs[0].tx.grossAmount // In lamports
+    const nftTransactionData = {
+      operation: "buy",
+        name: nftMeta.name,
+        slug: nftMeta.slug,
+        tokenId: tokenId,
+        owner: sellerId,
+        amount: grossAmount,
+        userAddress: userAddress
+        // toAddress: nftInfo[3],
+        
+    }
+
+    const resp = await constructNFTransaction(nftTransactionData);
+    console.log('this is resp', resp);
+
+    return { ...resp, type: 'nft_buy' };
+  } else if (context === "nft_sell") {
+    // TODO:
+  }
+  else if(context === 'transfer') { // polygon testnet
+    console.log('extracting transfer info');
+
+    const tokenSendInfo = [];
+    for(let i=0; i < sendTokenInfoExtractor.length; i++) {
+        const resp = await getResponse(sendTokenInfoExtractor[i].question, currentStep);
+        tokenSendInfo.push(resp);
+    }
+
+    // 0 -> name
+    // 1 -> amount
+    // 2 -> to address
+    const sendTokenMeta = supportTokenSend.filter(data => isWordSimilar(data.name, tokenSendInfo[0]));
+
+    if(sendTokenMeta.length === 0) return "Insufficient details for transfer";
+
+    const sendTransactionData = {
+        programAddress: sendTokenMeta[0].address,
+        name: sendTokenMeta[0].name,
+        fromAddress: userAddress,
+        amount: tokenSendInfo[1],
+        toAddress: tokenSendInfo[2]
+    };
+
+    console.log("sendTransactionData", sendTransactionData);
+    const sendTransactionResp = await constructSendTransaction(sendTransactionData);
+    console.log('sendTransactionResp', sendTransactionResp);
+
+    return { ...sendTransactionResp, type: 'transfer'};
+  } else if(context === 'staking') { // mumbai -> avalanche
+    let tokenAmount = 0;
+    // 0 -> token amount 
+    tokenAmount = await getResponse(bridgeInfoExtractor[0].question, currentStep);
+
+    if(tokenAmount === '-') {
+      return {
+        success: false,
+        transactions: []
+      }
+    }
+
+    const bridgeTransactionData = {
+      amount: tokenAmount,
+      userAddress
+    }
+
+    console.log('bridge data ', bridgeTransactionData);
+
+    let bridgeTxnResp = await contructBridgeTransactionForStaking(bridgeTransactionData);
+    console.log('txn ', bridgeTxnResp);
+    return { ...bridgeTxnResp, type: 'bridge' };
+  }
 };
 
-module.exports = { transpiler };
+module.exports = { transpiler }
+
