3f24aebb44d46a0d6465aa0246d879902f67b4ee
Long Nguyễn Giao Huỳnh <pierre@pierreneter.com>
2023-10-13 23:10:09 +0700

    build: fix ci of handler

--- k8s/save.sh
+++ k8s/save.sh
@@ -1,4 +1,3 @@
-# Copyright 2021 - 2023 Transflox LLC. All rights reserved.
 
 kubectl apply -f ./services.yml
 kubectl apply -f ./ingresses.yml
--- k8s/secrets/save.sh
+++ k8s/secrets/save.sh
@@ -1,3 +1,2 @@
-# Copyright 2021 - 2023 Transflox LLC. All rights reserved.
 
 kubectl apply -f ./gen.secrets-staging.yml
